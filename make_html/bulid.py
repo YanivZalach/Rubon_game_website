bf87054088b7b772deb5ee6eca6a85d8896eb361
YanivZalach <yzal2318@gmail.com>
2024-01-03 17:38:06 +0200

    Most of index.html and update to the build file

--- make_html/bulid.py
+++ make_html/bulid.py
@@ -12,6 +12,9 @@ creation_location = "../website"
 # Getting the footer and header from the listed files
 wrapper = []
 
+# List of all the files that we are going to create
+indexs = []
+
 try:
     wrapper.append(open("./parts/wrappers/header.html", "r").read())
     print("The header file was read")
@@ -26,8 +29,6 @@ try:
 except FileNotFoundError:
     print("Could not read the footer file")
 
-# List of all the files that we are going to create
-indexs = []
 
 # Looping through the list of pages and getting their content
 for root, dirs, files in os.walk("./parts/indexs"):
@@ -42,6 +43,49 @@ for root, dirs, files in os.walk("./parts/indexs"):
 if not os.path.isdir(creation_location):
     os.mkdir(creation_location)
 
+
+# Printing to the user that we created the file string
+print("\nCreating the file strings",end='\n\n')
+
+# Making the file strings
+for file_combo in indexs:
+    # The name and the file content from the file_combo
+    name = file_combo[0]
+    content = file_combo[1]
+
+    # Creating the file strings
+    try:
+        file_combo[1] = wrapper[0] + content + wrapper[1]
+        print(f"File string {name} wes created successfully")
+
+    except Exception as e:
+        print(f"Could not create '{name}' - {type(e).__name__}")
+
+
+# Printing to the user that we are adding the personal stylesheet
+print("\nAdding personal stylesheet",end='\n\n')
+
+# Adding personal stylesheet (stylesheet name: {name}_style.css)
+for file_combo in indexs:
+    # The name and the file content from the file_combo
+    name = file_combo[0]
+    content = file_combo[1]
+
+    # Adding the stylesheet
+    try:
+        # Locating the place to add the stylesheet: `<!-- Insert stylesheet -->`
+        add_index = content.find('<!-- Insert stylesheet -->')
+        # Getting the file name without the end
+        file_name = name[ : name.find('.') ]
+        # Adding to the content
+        file_combo[1]=content[:add_index] +f'<link rel="stylesheet" href="css/{file_name}_style.css">\n' + content[add_index:]
+        print(f"Added the stylesheet to {name}")
+
+    except Exception as e:
+        print(f"Could not find the index comment in '{name}' - {type(e).__name__}")
+    
+
+
 # Printing to the user that we are in the building stage
 print("\nBuilding the webpages",end='\n\n')
 
@@ -54,9 +98,7 @@ for file_combo in indexs:
     # Creating the files
     try:
         file = open(os.path.join(creation_location,name),"w")
-        file.write(wrapper[0])   # The header
         file.write(content)   # The content of that specific webpage
-        file.write(wrapper[1])   # The footer
         file.close()
         print(f"File {name} wes created successfully")
 
